40d6270099a2dc320eaeaa28d84a5b63cea1ec52
Yakumo Saki <yakumo@ziomatrix.org>
2018-12-27 22:10:23 +0900

    add drawBox, drawFrame, drawCircle

--- dumpdisplay.lua
+++ dumpdisplay.lua
@@ -27,42 +27,36 @@ disp:setFontMode(0)
 disp:setDrawColor(1)
 disp:setBitmapMode(0)
 
-disp:drawBox(0, 0, 10, 10)
+disp:drawFrame(90, 40, 10, 20)
+disp:drawBox(110, 40, 10, 20)
+
+disp:drawFrame(0, 0, 128, 64)
 disp:sendBuffer()
 
 -- https://github.com/olikraus/u8g2/wiki/fntlistall#6-pixel-height
 
-disp:setFont(u8x8_font_artossans8_r)
-
-disp:setFont(u8g2_font_ncenB14_tf);
-disp:setFontDirection(0);
-
-disp:setFont(u8g2_font_m2icon_5_tf)
-disp:setFont(u8g2_font_6x10_tf)
-
-disp:setFont(font_6x10_tf)
-
-disp:clearBuffer()
-disp:setFontMode(1)
-disp:setDrawColor(1)
--- disp:setFont("u8g2_font_6x10_tf")
-
-
 -- font
-disp:clearBuffer()
+-- disp:clearBuffer()
 
 -- OK
 disp:setFontMode(0)
 disp:setDrawColor(1)
 -- disp:setDrawColor(0)  BAD ! Black background
 disp:setFont(u8g2.font_6x10_tf)
-disp:drawStr(0, 40, "ABCDEFGabcdefg0123456789")
+disp:drawStr(2, 12, "ABCDEFGabcdefg0123456789")
 disp:sendBuffer()
 
 -- OK
 disp:setFontMode(0)
 disp:setDrawColor(1)
 disp:setFont(u8g2.font_unifont_t_symbols)
-disp:drawStr(0, 10, "AbcDefGhi0123456789")
+disp:drawStr(2, 28, "ABCDEFGabcdefg0123456789")
 disp:sendBuffer()
 
+-- circle
+disp:drawCircle(20, 40, 10, u8g2.DRAW_ALL)
+disp:drawCircle(30, 50, 10, u8g2.DRAW_ALL)
+disp:drawCircle(40, 40, 10, u8g2.DRAW_ALL)
+disp:drawCircle(50, 50, 10, u8g2.DRAW_ALL)
+disp:drawCircle(60, 40, 10, u8g2.DRAW_ALL)
+disp:sendBuffer()
